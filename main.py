0e05bd294937fe898d9cca233c781d36c7c16737
42ama <42ama@users.noreply.github.com>
2019-02-19 20:01:14 +0400

    Updated PlotOutput, loadJson method v003

--- main.py
+++ main.py
@@ -57,9 +57,10 @@ class HHStats:
 		self.namesList=[]
 		self.now = datetime.datetime.now()
 		self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0'}
-
+		self.incomedList=incomedList
 		url='https://samara.hh.ru/search/vacancy?clusters=true&enable_snippets=true&'
 		#["text=python&","text=c%23&","text=unity&","text=data+science&","text=javascript&"]
+		self.colorList=['blue','green','red','cyan','black','yellow','gray']
 		self.mainList = [[],
 					["","area=2&","&area=78&"],					
 					["","experience=noExperience&from=cluster_experience&","experience=between1And3&from=cluster_experience&"]
@@ -68,7 +69,7 @@ class HHStats:
 								["Россия", "Санкт-Петербург", "Самара"],
 								["","Нет опыта работы","1..3 года опыта"]]
 
-		compatibleList[0] = incomedList
+		compatibleList[0] = self.incomedList
 		for x in compatibleList[0]:
 			self.mainList[0].append('text='+urllib.parse.quote_plus(x)+'&')
 
@@ -85,6 +86,10 @@ class HHStats:
 			for y in x:
 				newName += " " + y
 			self.namesList.append(newName[1:])
+		self.langAllAny_exp = []
+		for c in range(len(self.namesList)):
+			if(c%(len(self.mainList)*len(self.mainList[1]))==0):
+				self.langAllAny_exp.append(c)
 		#self.urlList=["https://samara.hh.ru/search/vacancy?text=unity&only_with_salary=false&clusters=true&area=78&enable_snippets=true&","https://samara.hh.ru/search/vacancy?clusters=true&enable_snippets=true&text=C%23&experience=noExperience&from=cluster_experience"]
 		
 
@@ -119,8 +124,9 @@ class HHStats:
 		with open(fileName, 'w') as fp:
 			json.dump(self.dataDictionary, fp)
 
-	def loadJson(self):
-		fileName = 'data/'+self.now.strftime("%Y_%m_%d")+'.json'#___%H_%M добавить в название для часов и минут
+	def loadJson(self,fileName=None):
+		if(fileName==None):
+			fileName = 'data/'+self.now.strftime("%Y_%m_%d")+'.json'#___%H_%M добавить в название для часов и минут
 		with open(fileName, 'r') as fp:
 			self.dataDictionary = json.load(fp)
 
@@ -144,30 +150,32 @@ class HHStats:
 		#print(fig.axes)   # Список текущих областей рисования пуст
 		#print(type(fig))   # тип объекта Figure
 		#plt.scatter(1.0, 1.0)   # scatter - метод для нанесения маркера в точке (1.0, 1.0)
-		y=[]
-		markers=[]
-		for c in range(len(self.namesList)):
-			if(c%(len(self.mainList)*len(self.mainList[1]))==0):#длина 3
-				y.append(self.dataDictionary[self.namesList[c]])#для отрисовки на графике 1
-				markers.append(self.namesList[c].split()[:-1])#помимо одиночных листов появляется ["Data", 'science'] не будет работать для начала уберём сплит
-		y=np.array(y)
-		x=np.full(len(y),10)
-		#cc = plt.plot(x,y,color='grey', linewidth=2.5, linestyle='--')
-		plt.grid(b=True,alpha=0.3)
-		plt.scatter(x, y, marker='.', s=40, color='red')
+		c=0
 		plt.title('Тренды по языкам в России за последние 10 дней\n(абсолютная статистика)')
 		plt.xlabel('Дни')
 		plt.ylabel('Кол-во вакансий')
-		#plt.plot(x,y)
-		
-		# После нанесения графического элемента в виде маркера
-		# список текущих областей состоит из одной области
-		#print (fig.axes)
-		# смотри преамбулу
+		plt.grid(b=True,alpha=0.3)
+		x=np.array(range(0,-len(self.dataList),-1))
+		#ax = fig.add_subplot(111)
+		c=0
+		for oneList in self.dataList:
+			y=np.array(oneList)
+			plt.plot(x,y,color=self.colorList[c], linewidth=1, linestyle='--', label=self.incomedList[c])
+			plt.scatter(x, y, marker='.', s=20, color=self.colorList[c])
+			#plt.text(-0.5, oneList[0]+600, self.incomedList[c], fontsize=12, bbox=dict(edgecolor='w', color='w'))
+			#ax.plot(x+1, y, color=self.colorList[c], linewidth=1, label=self.incomedList[c])
+			c+=1
+		#ax.grid(True)
+		plt.legend(loc='upper right', bbox_to_anchor=(1.12, 0.95))
+		plt.show()
+		#cc = plt.plot(x,y,color='grey', linewidth=2.5, linestyle='--')
+		#plt.scatter(x, y, marker='.', s=40, color='red')
+
 		#save(name='pic_1_4_1', fmt='pdf')
 		#save(name='pic_1_4_1', fmt='png')
 		#plt.savefig('{}.{}'.format('pic_1_4_1', 'png'))
-		plt.show()
+		
+
 	def getNlast(self, n=10):
 		list_of_files = glob.glob('data/*.json') # * means all if need specific format then *.csv
 		fromLastToFirst = []
@@ -179,6 +187,21 @@ class HHStats:
 			list_of_files.remove(latest_file)
 		return fromLastToFirst
 
+	def getDataFromJson(self,listFiles):
+		#['data\\2019_02_19.json', 'data\\2019_02_18.json'...]
+		self.dataList=[]
+		for x in listFiles:
+			self.loadJson(x)
+			tempList=[]
+			for c in self.langAllAny_exp:
+				tempList.append(self.dataDictionary[self.namesList[c]])
+			self.dataList.append(tempList)
+		self.dataList.reverse()#originaly: 0-newest..n-oldest, after reverse: 0-oldest..n-newest
+		self.dataList=list(map(list, zip(*self.dataList)))#each sublist is принадлежит одной линии
+		
+
+
+
 print("loaded")
 if __name__ == '__main__':
 	print("also loaded as main")
@@ -186,5 +209,5 @@ if __name__ == '__main__':
 	#Sample.collectData()
 	#Sample.saveJson()
 	#Sample.loadJson()
-	#Sample.drawingPlot()
-	print(Sample.getNlast())
+	Sample.getDataFromJson(Sample.getNlast())
+	Sample.drawingPlot()
